4f9b6e5b61a68ac32445365377b2f9459f39ef48
khoidd <khoidd@vng.com.vn>
2020-10-16 09:43:22 +0700

    fix something

--- src/hog.py
+++ src/hog.py
@@ -1,7 +1,6 @@
-from skimage.io import imread, imshow
+from skimage.io import imread
 from skimage.transform import resize
 from skimage.feature import hog
-from skimage import exposure
 import matplotlib.pyplot as plt
 
 
